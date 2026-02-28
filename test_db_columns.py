a8a196c5876bf48059ffa2a98bd3b0bcc015aa0f
RAKESH <rakee4407@gmail.com>
2026-02-28 17:52:58 +0530

    Delete test_db_columns.py

--- test_db_columns.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import pandas as pd
-
-conn = sqlite3.connect("database/student_system.db")
-df = pd.read_sql_query("SELECT * FROM students", conn)
-conn.close()
-
-print(df.columns.tolist())
-print(df.head())
