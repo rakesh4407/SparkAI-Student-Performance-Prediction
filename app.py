4540befd4644d967d3d1345bb7d365435d71ad17
RAKESH <rakee4407@gmail.com>
2026-04-01 12:30:18 +0530

    fix: update app.py port for Render

--- app.py
+++ app.py
@@ -1634,4 +1634,4 @@ def test_db():
 
 if __name__ == "__main__":
     init_db_and_admin()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
